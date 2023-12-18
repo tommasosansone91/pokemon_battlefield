d287d468128b83b0f390cb38514093b09a70d5e3
Tommaso Sansone <tommaso.sansone91@virgilio.it>
2023-12-18 18:53:25 +0100

    beautified

--- app_battle_simulator/urls.py
+++ app_battle_simulator/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     # # path, vista, nome di richiamo
 
     path('', views.home, name="home"),
-
     
-path('api/launch_battle/<int:pkmn_id1>/<int:pkmn_id2>/', api.launch_battle, name="launch_battle"),
+    path(
+        'api/launch_battle/<int:pkmn_id1>/<int:pkmn_id2>/', 
+         api.launch_battle, 
+         name="launch_battle"
+         ),
     
     # mantieni lo standard di nomenclatura tra i tre termini
 ]
